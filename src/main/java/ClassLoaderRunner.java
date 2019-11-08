8b13e672fd6d3431d17daf85907cc1a9c641c6f4
Alvin Huynh <avh670@gmail.com>
2019-11-07 21:27:31 -0600

    Modified CCL and CCRunner to show classes that are loaded

--- src/main/java/ClassLoaderRunner.java
+++ src/main/java/ClassLoaderRunner.java
@@ -1,7 +1,22 @@
+import java.lang.reflect.Method;
+
 public class ClassLoaderRunner {
     static public void main(String[] args) {
-        String classPath = args[0];
-        String mainClass = args[1];
-        System.arraycopy();
+        try {
+            String mainClass = args[0];
+            String[] arguments = new String[args.length-1];
+            System.arraycopy(args, 1, arguments, 0, arguments.length);
+            CustomClassLoader ccl = new CustomClassLoader();
+
+            Class mainClassObject = ccl.loadClass(mainClass);
+            Class[] mainArgType = {String[].class};
+            Method main = mainClassObject.getMethod("main", mainArgType);
+            Object[] argsArray = {arguments};
+            main.invoke(null, argsArray);
+
+        } catch (Exception e) {
+            System.out.println("Error: Incorrect parameters.");
+            System.out.println("java ClassLoaderRunner mainclass args");
+        }
     }
 }
