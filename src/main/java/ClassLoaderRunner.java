a1a3534f8541770daf48ae349bd9d809f7d866b1
Alvin Huynh <avh670@gmail.com>
2019-11-07 20:39:44 -0600

    Initial commit of class loader runner

---
+++ src/main/java/ClassLoaderRunner.java
@@ -0,0 +1,7 @@
+public class ClassLoaderRunner {
+    static public void main(String[] args) {
+        String classPath = args[0];
+        String mainClass = args[1];
+        System.arraycopy();
+    }
+}
